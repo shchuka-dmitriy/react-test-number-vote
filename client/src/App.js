38474ac05dd6a730f2d0a6ee9ae94a33393bcb21
shchuka-dmitriy <dmitriy.shchuka.rule@gmail.com>
2020-11-19 03:57:49 +0200

    Client part. Change App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,12 @@
 import React, {Component} from 'react';
 import {Router, Route, Switch} from 'react-router-dom';
+import Home from './pages/Home/Home';
 import browserHistory from './browserHistory';
 import './App.css';
 import './reset.css';
+import './assets/styles/react-confirm-alert.css';
+import VotePage from "./pages/VotePage/VotePage";
+import LogsPage from "./pages/LogsPage/LogsPage";
 
 class App extends Component {
   render() {
@@ -10,6 +14,8 @@ class App extends Component {
         <Router history={browserHistory}>
           <Switch>
             <Route exact path='/' component={Home}/>
+            <Route exact path='/votePage' component={VotePage}/>
+            <Route exact path='/logsPage' component={LogsPage}/>
           </Switch>
         </Router>
     );
